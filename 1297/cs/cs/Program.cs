5d5889dc914f3ccde9a86c84a591d5b252801853
Bulat Gabdrakhmanov <bulat.gab2000@gmail.com>
2021-02-16 22:49:45 +0300

    Решил 1297 (c#)

---
+++ 1297/cs/cs/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace cs
+{
+    class Program
+    {
+        static string GetPalindromeSubstring(string text)
+        {
+            var result = text[0].ToString();
+
+            for (var center = 0; center < text.Length; center++)
+            {
+                var left = center;
+                var right = center;
+
+                while (left > 0 && text[left - 1] == text[center]) left--;
+                while (right < text.Length - 1 && text[right + 1] == text[center]) right++;
+
+                while (left >= 1 && right < text.Length - 1 && text[left - 1] == text[right + 1])
+                {
+                    left--;
+                    right++;
+                }
+
+                if (right - left + 1 > result.Length)
+                {
+                    result = text.Substring(left, right - left + 1);
+                }
+            }
+
+            return result;
+        }
+        
+        static void Main(string[] args)
+        {
+            var message = Console.ReadLine();
+            var result = message.Length <= 1
+                ? message
+                : GetPalindromeSubstring(message);
+
+            Console.WriteLine(result);
+        }
+    }
+}
