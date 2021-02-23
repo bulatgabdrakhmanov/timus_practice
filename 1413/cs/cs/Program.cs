6ac06b5c01bd8daa9818c1f7850390995284924b
Bulat Gabdrakhmanov <bulat.gab2000@gmail.com>
2021-02-24 00:06:41 +0300

    Решил 1413 (c#)

---
+++ 1413/cs/cs/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace cs
+{
+    class Program
+    {
+        static string CalculateCoors(string buttons)
+        {
+            double x = 0;
+            double y = 0;
+            double diagonalDistance = 1 / Math.Sqrt(2);
+
+            foreach (var button in buttons)
+            {
+                if (button == '0') break;
+
+                switch (button)
+                {
+                    case '1':
+                        y -= diagonalDistance;
+                        x -= diagonalDistance;
+                        break;
+                    
+                    case '2':
+                        y--;
+                        break;
+                    
+                    case '3':
+                        y -= diagonalDistance;
+                        x += diagonalDistance;
+                        break;
+                    
+                    case '4':
+                        x--;
+                        break;
+
+                    case '6':
+                        x++;
+                        break;
+                    
+                    case '7':
+                        y += diagonalDistance;
+                        x -= diagonalDistance;
+                        break;
+                    
+                    case '8':
+                        y++;
+                        break;
+                    
+                    case '9':
+                        y += diagonalDistance;
+                        x += diagonalDistance;
+                        break;
+                }
+            }
+            
+            return x.ToString("n10").Replace(",", ".") + 
+                   " " + 
+                   y.ToString("n10").Replace(",", ".");
+        }
+        
+        static void Main(string[] args)
+        {
+            string buttons = Console.ReadLine();
+            Console.WriteLine(CalculateCoors(buttons));
+        }
+    }
+}
